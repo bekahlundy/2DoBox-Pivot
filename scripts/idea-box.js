e77847cb655c5bb218488b1e22af1e5be08d9fb3
Eric Sayler <easayler@gmail.com>
2016-10-25 10:50:33 -0600

    remove placeholder cards, add prepend card feature, add delete card feature, start upvote,downvote feature

--- scripts/idea-box.js
+++ scripts/idea-box.js
@@ -1,9 +1,11 @@
 var count = 0;
 
+var qualityArray = ['swill', 'plausible', 'genius'];
+
 var titleField = $('.title-field');
 var bodyField = $('.body-field');
 var inputFields = $('.title-field, .body-field');
-var list = $('.idea-list');
+var ideaList = $('.idea-list');
 var saveButton = $('.save-button');
 var searchField = $('.search-bar');
 
@@ -15,3 +17,42 @@ function Idea() {
   this.body = body;
   this.quality = quality;
 }
+
+saveButton.on('click', function () {
+  var title = titleField.val();
+  var body = bodyField.val();
+  addCardToList(title, body);
+  titleField.focus();
+  clearInput();
+});
+
+function addCardToList(title, body) {
+  var quality = qualityArray[0];
+  var newCard =
+    `<article class="card">
+      <h2 class="card-title">${title}</h2>
+      <input class="card-button delete" type="button" name="name" value="">
+      <p class="card-body">${body}</p>
+      <input class="card-button upvote" type="button" name="name" value="">
+      <input class="card-button downvote" type="button" name="name" value="">
+      <div class="card-quality">quality: <span class="quality-value">${quality}</span></div>
+    </article>`;
+  ideaList.prepend(newCard);
+}
+
+function clearInput() {
+  titleField.val('');
+  bodyField.val('');
+}
+
+ideaList.on('click', '.delete', function () {
+  $(this).parent().remove();
+});
+
+ideaList.on('click', '.upvote', function () {
+  $(this).parent().
+});
+
+ideaList.on('click', '.downvote', function () {
+  $(this).parent().
+});
