bca6fdd73db62cd1afb5eed30846fcdaa3771c03
bekahlundy <lundyrebekah@gmail.com>
2016-12-04 19:57:48 -0700

    attempt to make testing work, change colors to be better with aXe

--- test/index.js
+++ test/index.js
@@ -1,7 +1,11 @@
-const assert = require('chai').assert
+import Card from '../lib/todo-box.js';
+const assert = require('chai').assert;
 
 describe('our test bundle', function () {
   it('should work', function () {
-    assert(true)
-  })
-})
+    assert(true);
+  });
+  it('should be a function', function () {
+    assert.isFunction(Card);
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader'},
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!sass" },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
